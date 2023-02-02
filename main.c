684dbbc135bd83be22f11bb47d4cb9354028eb72
Sukhendra S Lodhi <95135906+sukhendralodhi@users.noreply.github.com>
2023-02-02 04:59:14 +0000

    convert temrature using function

--- main.c
+++ main.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 
+// function 
+float fahrenheit_to_celcius(float fahrenheit) {
+  return ((fahrenheit-32.0)*5.0/9.0);
+}
+// driver code 
 int main(void) {
-  float fahrenheit, temprature;
+  float fahrenheit;
   printf("Enter the temprature in fahrenheit: ");
-  scanf("%f",&fahrenheit);
+  scanf("%f", &fahrenheit);
 
-  // formula 
-  temprature = ((fahrenheit - 32.0) * 5.0/9.0);
-  
-  printf("Temprature in degree celcius: %.2f", temprature);
+  // passing parameter to function 
+  printf("Temprature in degree celcius: %.2f", fahrenheit_to_celcius(fahrenheit));
   return 0;
 }
